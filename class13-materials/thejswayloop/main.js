362519dc47bf9bb62d6acce485b44be9aba7a51e
Kiarra Gibbs <you@example.com>
2024-01-14 09:00:21 -0500

    Changed innertext section of angry parent

---
+++ class13-materials/thejswayloop/main.js
@@ -0,0 +1,27 @@
+//carousel
+for(let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+let i = 1;
+while(i <= 10) {
+    console.log(i);
+    i++;
+}
+
+let num = prompt('Enter a number');
+let y = num - 1;
+
+for (let i = num; i <= i+10; i++) {
+    
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+    } else {
+        console.log(`${i} is odd`)
+    }
+}
+
+// let entered = prompt('Show a number');
+// let notTheOne = prompt('Enter a new number')
+
+// while(entered !== )
